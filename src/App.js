57a91016d5481acc8ff95b68a8d1e878c4f65623
cj <cj@neonmoneyclub.com>
2022-03-28 14:57:00 -0700

    login page created

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import React from 'react';
 import Login from './components/login';
 import Notepad from './components/notepad';
 import './App.css';
-import {
-  Route,
-  BrowserRouter as Router,
-} from "react-router-dom";
 
 export default function App() {
   return (
     <div>
-       <Router>
-          <div>
-           <Route exact path="/">
-              < Login />
-            </Route>
-            <Route exact path="/notepad">
-              < Notepad />
-            </Route>
-       </div>
-    </Router>
+      {/* < Notepad /> */}
+      < Login />
     </div>
-   
   );
 }
